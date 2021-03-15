7d3dc55ec38463ba90e70cbc9789b50dcb3f59b1
pandaCure <zhangzhongyou@kuaishou.com>
2021-03-15 16:46:16 +0800

    feat: 新增eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    extends: [
+        'alloy',
+        'alloy/typescript',
+        'alloy/react',
+    ],
+    env: {
+        // Your environments (which contains several predefined global variables)
+        //
+        // browser: true,
+        // node: true,
+        // mocha: true,
+        // jest: true,
+        // jquery: true
+    },
+    globals: {
+        // Your global variables (setting to false means it's not allowed to be reassigned)
+        //
+        // myGlobal: false
+    },
+    rules: {
+        // Customize your rules
+        '@typescript-eslint/no-empty-interface': 0
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // max 120 characters per line
+    printWidth: 120,
+    // use 2 spaces for indentation
+    tabWidth: 2,
+    // use spaces instead of indentations
+    useTabs: false,
+    // semicolon at the end of the line
+    semi: false,
+    // use single quotes
+    singleQuote: true,
+    // object's key is quoted only when necessary
+    quoteProps: 'as-needed',
+    // use double quotes instead of single quotes in jsx
+    jsxSingleQuote: false,
+    // no comma at the end
+    trailingComma: false
+}
